0df39dbfdc3f6c1522b474c78d0b5ac1351fcff2
thelegendarybacon <abakker10@gmail.com>
2023-04-07 16:22:39 -0500

    added headers and description

---
+++ z1856158_project4.cc
@@ -0,0 +1,14 @@
+/********************************************************************
+CSCI 480 - Assignment 4 - Spring 2023
+
+Progammer: Alec Bakker
+Section: 0001
+TA: Daniel Feller 
+Date Due:  4/11/2023
+
+Purpose: Implements a solution to the reader/writer problem using
+         the pthread library.
+*********************************************************************/
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
